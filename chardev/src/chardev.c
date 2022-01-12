f04ae7f774b12a91719f7e24bd966c891bd81766
Kubuntu <kubuntu@kubuntu2110.linuxvmimages.local>
2022-01-12 20:46:43 +0200

    chardev: add mutex example.

--- chardev/src/chardev.c
+++ chardev/src/chardev.c
@@ -16,6 +16,7 @@
 #include <linux/jiffies.h>
 #include <linux/timer.h>
 #include <linux/interrupt.h>
+#include <linux/mutex.h>
 #include <asm/io.h>
 
 
@@ -25,16 +26,24 @@ static struct timer_list char_timer;
 void timer_callback(struct timer_list * timer);
 void timer_callback(struct timer_list * timer)
 {
-	printk(KERN_INFO "timer_callback at %lu jiffies == %d ms\n", 
-		jiffies, jiffies_to_msecs(jiffies));
+	// printk(KERN_INFO "timer_callback at %lu jiffies == %d ms\n", jiffies, jiffies_to_msecs(jiffies));
 	mod_timer(&char_timer, jiffies+msecs_to_jiffies(TIMEOUT_MS));
-
 }
 
  
 // kthread
 static struct task_struct *char_thread1;
 int char_thread_fn1(void * p);
+
+
+// mutex 
+// struct mutex char_mutex;
+DEFINE_MUTEX(char_mutex);
+unsigned long char_mutex_val = 0;
+static struct task_struct *char_mutex_thread1;
+static struct task_struct *char_mutex_thread2;
+int char_mutex_thread_fn1(void * p);
+int char_mutex_thread_fn2(void * p);
 
 
 // spinlock
@@ -52,11 +61,11 @@ DECLARE_TASKLET(tasklet1, tasklet_fn1);
 struct tasklet_struct * tasklet2;
 void tasklet_fn1(struct tasklet_struct * data) 
 {
-	printk(KERN_INFO "Executing tasklet 1: data = %ld\n", data->data);
+	// printk(KERN_INFO "Executing tasklet 1: data = %ld\n", data->data);
 }
 void tasklet_fn2(unsigned long data)
 {
-	printk(KERN_INFO "Executing tasklet 2: data = %ld\n", data);
+	// printk(KERN_INFO "Executing tasklet 2: data = %ld\n", data);
 }
 
 
@@ -67,8 +76,8 @@ static irqreturn_t irq_handler(int irq, void* dev_id)
 {
 	static uint8_t i = 0;
 	++i;
-	if (i % 10 == 0) {
-		printk(KERN_INFO "Keyboard IRQ occured %d times\n", i);
+	if (i % 20 == 0) {
+		printk(KERN_INFO "Keyboard IRQ: occured %d times\n", i);
 		tasklet_schedule(&tasklet1);
 	}
 	return IRQ_HANDLED;
@@ -172,6 +181,7 @@ static int  __init chardrv_init(void)
 		goto r_device;
 	}
 
+
 	// kthread w/ spinlock
 	// char_thread2 = kthread_run(char_thread_fn2, NULL, "char_thread2");
 	char_thread2 = kthread_create(char_thread_fn2, NULL, "char_thread2");
@@ -191,6 +201,19 @@ static int  __init chardrv_init(void)
 	}
 
 
+	// kthread w/ mutex
+	mutex_init(&char_mutex);
+	char_mutex_thread1 = kthread_run(char_mutex_thread_fn1, NULL, "char_mutex_thread1");
+	if (char_mutex_thread1 == NULL) {
+		printk(KERN_ERR "\tERROR creating char_mutex_thread 1\n");
+		goto r_device;
+	}
+	char_mutex_thread2 = kthread_run(char_mutex_thread_fn2, NULL, "char_mutex_thread2");
+	if (char_mutex_thread2 == NULL) {
+		printk(KERN_ERR "\tERROR creating char_mutex_thread 2\n");
+		goto r_device;
+	}
+	
 	return 0;
 
 r_irq:
@@ -205,9 +228,11 @@ r_class:
 
 static void __exit chardrv_exit(void)
 {
-	kthread_stop(char_thread1);
-	kthread_stop(char_thread2);
+	kthread_stop(char_mutex_thread2);
+	kthread_stop(char_mutex_thread1);
 	kthread_stop(char_thread3);
+	kthread_stop(char_thread2);
+	kthread_stop(char_thread1);
 	tasklet_kill(tasklet2);
 	del_timer(&char_timer);
 	free_irq(IRQ_NO, (void*)(irq_handler));
@@ -242,7 +267,7 @@ static int my_close(struct inode * inode, struct file * file)
 static ssize_t my_read( struct file * file, char __user * buf, size_t len, loff_t * off)
 {
 	copy_to_user(buf, kernel_buffer, MEM_SIZE);
-	printk(KERN_INFO "copy_to_user %u bytes\n", MEM_SIZE);
+	printk(KERN_INFO "copy_to_user   %u bytes\n", MEM_SIZE);
 	return MEM_SIZE;
 }
 
@@ -273,11 +298,37 @@ static long my_ioctl(struct file * file, unsigned int cmd, unsigned long arg)
 	return 0;
 }
 
+int char_mutex_thread_fn1(void * p)
+{
+	while(!kthread_should_stop()) {
+		// mutex_lock_interruptible(&char_mutex);
+		mutex_lock(&char_mutex);
+		++char_mutex_val;
+		printk(KERN_INFO "mutex: inside thread 1 ... %lu\n", char_mutex_val);
+		mutex_unlock(&char_mutex);
+		msleep(1000);
+	}
+	return 0;
+}
+
+int char_mutex_thread_fn2(void * p)
+{
+	while(!kthread_should_stop()) {
+		// mutex_lock_interruptible(&char_mutex);
+		mutex_lock(&char_mutex);
+		++char_mutex_val;
+		printk(KERN_INFO "mutex: inside thread 2 ... %lu\n", char_mutex_val);
+		mutex_unlock(&char_mutex);
+		msleep(1000);
+	}
+	return 0;
+}
+
 int char_thread_fn1(void * p)
 {
 	int i = 0;
 	while(!kthread_should_stop()) {
-		printk(KERN_INFO "Inside kthread ... %d\n", i);
+		// printk(KERN_INFO "Inside kthread ... %d\n", i);
 		++i;
 		msleep(1000);
 	}
@@ -288,12 +339,11 @@ int char_thread_fn2(void * p)
 {
 	while(!kthread_should_stop()) {
 		if (!spin_is_locked(&char_spinlock)) {
-			printk(KERN_INFO "spin: unlocked\n");
+			// printk(KERN_INFO "spin: unlocked\n");
 			spin_lock(&char_spinlock);
 			if (spin_is_locked(&char_spinlock)) {
 				static uint32_t spinlock_var = 0;
-				printk(KERN_INFO "spin: locked\n");
-				printk(KERN_INFO "spinlock_var 2 = %d\n", spinlock_var);
+				// printk(KERN_INFO "spin:   locked: spinlock_var 2 = %d\n", spinlock_var);
 				++spinlock_var;
 				spin_unlock(&char_spinlock);
 				msleep(1000);
@@ -308,12 +358,11 @@ int char_thread_fn3(void * p)
 {
 	while(!kthread_should_stop()) {
 		if (!spin_is_locked(&char_spinlock)) {
-			printk(KERN_INFO "spin: unlocked\n");
+			// printk(KERN_INFO "spin: unlocked\n");
 			spin_lock(&char_spinlock);
 			if (spin_is_locked(&char_spinlock)) {
 				static uint32_t spinlock_var = 0;
-				printk(KERN_INFO "spin: locked\n");
-				printk(KERN_INFO "spinlock_var 3 = %d\n", spinlock_var);
+				// printk(KERN_INFO "spin:   locked: spinlock_var 3 = %d\n", spinlock_var);
 				++spinlock_var;
 				spin_unlock(&char_spinlock);
 				msleep(1000);
